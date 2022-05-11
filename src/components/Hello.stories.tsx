a4b354e70f38524a05e852d0dd4d1fc7c1f4fb4e
eddyzera <edgar.nascimento@newgo.com.br>
2022-05-11 14:06:39 -0300

    [chore]: configuration storybook

--- src/components/Hello.stories.tsx
+++ src/components/Hello.stories.tsx
@@ -7,7 +7,7 @@ import { Hello } from '.';
 export default {
   title: 'Example/Button',
   component: Hello,
-};
+} as ComponentMeta<typeof Hello>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: ComponentStory<typeof Hello> = (args) => <Hello {...args} />;
